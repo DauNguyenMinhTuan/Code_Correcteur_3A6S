22fb849e7ad5e074e3ee211ce31a6aa994d73bb8
DauNguyenMinhTuan <dnmtuan27112002@gmail.com>
2023-06-14 14:57:31 +0200

    ethiquete

--- Main.java
+++ Main.java
@@ -22,28 +22,50 @@ public class Main {
     public static void main(String[] arg) {
 
         // test Matrix
+        System.out.println("Test matrice");
         byte[][] tab = { { 1, 0, 0 }, { 0, 1, 0 }, { 0, 0, 1 } };
         Matrix m = new Matrix(tab);
         m.display();
 
+        System.out.println("--------------------------------------------------------------\n");
+
         // test addition
+        System.out.println("Test addition");
         byte[][] tab1 = { { 1, 0, 1 }, { 0, 1, 0 }, { 1, 0, 1 } };
         Matrix m1 = new Matrix(tab1);
         byte[][] tab2 = {{0, 1, 0}, {1, 0, 1}, {0, 1, 0}};
         Matrix m2 = new Matrix(tab2);
+        System.out.println("Matrice m1");
+        m1.display();
+        System.out.println("Matrice m2");
+        m2.display();
+        System.out.println("Résultat de m1 + m2");
         m1.add(m2).display();
 
+        System.out.println("--------------------------------------------------------------\n");
+
         // test multiplication
+        System.out.println("Test multiplication");
         byte[][] tab3 = { { 1, 0, 1 }, { 1, 1, 0 } };
         Matrix m3 = new Matrix(tab3);
         byte[][] tab4 = { { 0, 1 }, { 1, 1 }, { 1, 0 } };
         Matrix m4 = new Matrix(tab4);
+        System.out.println("Matrice m3");
+        m3.display();
+        System.out.println("Matrice m4");
+        m4.display();
+        System.out.println("Résultat de m3 x m4");
         m3.multiply(m4).display();
+
+        System.out.println("--------------------------------------------------------------\n");
 
         // test transpose
         byte[][] tab5 = { { 1, 0, 1 }, { 1, 1, 0 } };
         Matrix m5 = new Matrix(tab5);
+        System.out.println("Test transpose");
+        System.out.println("Matrice m5");
         m5.display();
+        System.out.println("Matrice m5 transposé");
         m5.transpose().display();
 
         // check data - not correct size
@@ -51,6 +73,10 @@ public class Main {
         // System.out.println(check.getRows());
         // System.out.println(check.getCols());
         // check.display();
+
+        System.out.println("--------------------------------------------------------------");
+        System.out.println("                           1e Tâche                           ");
+        System.out.println("--------------------------------------------------------------\n");
 
         // 1e tache
         // test sur petit matrice
@@ -70,10 +96,16 @@ public class Main {
         Matrix X = U.multiply(hbase.sysTransform().genG());
         X.display();
 
+        System.out.println("--------------------------------------------------------------");
+        System.out.println("                           2e Tâche                           ");
+        System.out.println("--------------------------------------------------------------\n");
+
         // 2e tache
         System.out.println("Le graphe de Tanner de H");
         TGraph tGraph = new TGraph(hbase, 3, 4);
         tGraph.display();
+
+        System.out.println("--------------------------------------------------------------\n");
 
         byte[][] tabe1 = { { 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         0 } };
@@ -81,10 +113,14 @@ public class Main {
         Matrix y1 = X.add(e1);
         System.out.println("Le mot y1");
         y1.display();
+        System.out.println("Transpose de syndrome de y1");
+        hbase.multiply(y1.transpose()).transpose().display();
         System.out.println("Le mot y1 corrigé");
         tGraph.decode(y1, 100).display();
         System.out.println("Le mot x original");
         X.display();
+
+        System.out.println("--------------------------------------------------------------\n");
 
         byte[][] tabe2 = { { 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         0 } };
@@ -92,10 +128,14 @@ public class Main {
         Matrix y2 = X.add(e2);
         System.out.println("Le mot y2");
         y2.display();
+        System.out.println("Transpose de syndrome de y2");
+        hbase.multiply(y2.transpose()).transpose().display();
         System.out.println("Le mot y2 corrigé");
         tGraph.decode(y2, 100).display();
         System.out.println("Le mot x original");
         X.display();
+
+        System.out.println("--------------------------------------------------------------\n");
 
         byte[][] tabe3 = { { 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0,
         0 } };
@@ -103,10 +143,14 @@ public class Main {
         Matrix y3 = X.add(e3);
         System.out.println("Le mot y3");
         y3.display();
+        System.out.println("Transpose de syndrome de y3");
+        hbase.multiply(y3.transpose()).transpose().display();
         System.out.println("Le mot y3 corrigé");
         tGraph.decode(y3, 100).display();
         System.out.println("Le mot x original");
         X.display();
+
+        System.out.println("--------------------------------------------------------------\n");
 
         byte[][] tabe4 = { { 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
         0 } };
@@ -114,10 +158,16 @@ public class Main {
         Matrix y4 = X.add(e4);
         System.out.println("Le mot y4");
         y4.display();
+        System.out.println("Transpose de syndrome de y4");
+        hbase.multiply(y4.transpose()).transpose().display();
         System.out.println("Le mot y4 corrigé");
         tGraph.decode(y4, 100).display();
         System.out.println("Le mot x original");
         X.display();
+
+        System.out.println("--------------------------------------------------------------");
+        System.out.println("                           3e Tâche                           ");
+        System.out.println("--------------------------------------------------------------\n");
 
         // 3e tache
         Matrix H = loadMatrix("data/Matrix-2048-6144-5-15", 2048, 6144);
